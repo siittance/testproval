1fcc3df1e8b0ca2aa6195c3a8ccfa82bb85beccc
siittance <144900566+siittance@users.noreply.github.com>
2024-03-27 23:56:23 +0300

    jj

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Testiki
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace Testiki
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Redak redak = new Redak();
+        public static string parol { get; set; }
+        public MainWindow()
+        {
+                InitializeComponent();
+
+        }
+
+        private void Redak(object sender, RoutedEventArgs e)
+        {
+            parol = Parol.Text;
+            Parol.IsEnabled = true;
+            if (parol == "kop")
+            {
+                redak.Show();
+                Close();
+            }
+        }
+
+        private void Start(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
